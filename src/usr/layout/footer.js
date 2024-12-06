838a4047ba487830639c2af11393467dba354199
Julian Bogdani <jbogdani@gmail.com>
2024-12-06 11:41:50 +0100

    Some text added

--- src/usr/layout/footer.js
+++ src/usr/layout/footer.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import styled from "styled-components"
-import { Link } from "gatsby"
 import { Container } from "react-bootstrap"
 
 const FooterSection = () => {
@@ -10,8 +9,12 @@ const FooterSection = () => {
       <Container>
         <div className="d-flex flex-row align-items-center mb-3">
           <div className="p-3">
-            Powered by<br />
-            <Link to="/">
+            Powered by
+            <br />
+            <a
+              href="https://github.com/lab-archeologia-digitale/sCMS/"
+              title="s:CMS a research data oriented CMS build by LAD"
+            >
               <StaticImage
                 src="../../images/scms-lad.png"
                 width={100}
@@ -20,11 +23,11 @@ const FooterSection = () => {
                 alt="LAD: Laboratorio di Archeologia Digitale alla Sapienza"
                 className="img-fluid"
               />
-            </Link>
+            </a>
           </div>
           <div className="p-3 border-start border-primary">
             <p className="p-s-3">
-              Build with ♥ by {' '}
+              Build with ♥ by{" "}
               <a
                 href="https://lad.saras.uniroma1.it"
                 target="_blank"
@@ -33,9 +36,16 @@ const FooterSection = () => {
                 LAD: Laboratorio di Archeologia Digitale alla Sapienza
               </a>
             </p>
-              <hr />
-            <p>Copyright and credits: Italian Archaeological Mission in Afghanistan</p>
-            <p><a href="mailto:afiligenzi@unior.it">afiligenzi@unior.it</a></p>
+            <hr />
+            <p>
+              Copyright and credits: 2024{" "}
+              {new Date().getFullYear() > 2024 &&
+                `- ${new Date().getFullYear()}`}{" "}
+              Italian Archaeological Mission in Afghanistan
+            </p>
+            <p>
+              Contact: <a href="mailto:afiligenzi@unior.it">afiligenzi@unior.it</a>
+            </p>
           </div>
         </div>
       </Container>
